bcbdbcd2e930a5fa593bc4f3138559b13cc2759b
dimaspaiva <dimasalpaiva@gmail.com>
2020-05-28 13:26:25 -0300

    start

--- ball.js
+++ ball.js
@@ -1,24 +1,95 @@
 class Ball {
-  constructor(x, y, size) {
-    this.x = x
-    this.y = y
-    this.size = size
+  constructor(radius, id, desease = false) {
+    this.x = Math.ceil(Math.random() * (300 - radius))
+    this.y = Math.ceil(Math.random() * (300 - radius))
+    this.id = id
+
+    this.xVelocity = 0.15 * this.randomDirection()
+    this.yVelocity = 0.15 * this.randomDirection()
+
+    this.moves = 0
+
+    this.radius = radius
+
+    this.isInfected = desease
   }
 
   draw = () => {
+    this.person()
+    // this.range(3 * this.radius)
+  }
+
+  person = () => {
     noStroke()
-    fill(100, 200, 120)
-    ellipse(this.x, this.y, this.size)
+
+    if (this.isInfected) {
+      fill(255, 120, 120)
+    } else {
+      fill(90, 90, 200)
+    }
+    ellipse(this.x, this.y, this.radius * 2)
+  }
+
+  range = () => {
     noFill()
-    stroke(255, 0, 0)
-    rect(this.x - this.size / 2, this.y - this.size / 2, this.size)
+    stroke(255, 0, 0, 120)
+    ellipse(this.x, this.y, (this.radius + this.radius * 1.2) * 2)
   }
 
   move = () => {
-    if (this.x <= 300 - this.size / 2) {
-      console.log(this.x)
-      this.x += 2
+    if (this.x >= 300 - this.radius) {
+      this.x = 300 - this.radius
+      this.xVelocity *= -1
+    } else if (this.x <= 0 + this.radius) {
+      this.x = 0 + this.radius
+      this.xVelocity *= -1
     }
+
+    if (this.y >= 300 - this.radius) {
+      this.y = 300 - this.radius
+      this.yVelocity *= -1
+    } else if (this.y < 0 + this.radius) {
+      this.y = 0 + this.radius
+      this.yVelocity *= -1
+    }
+
+    this.x += this.xVelocity
+    this.y += this.yVelocity
+
+    if (this.moves > 40) {
+      if (this.randomDirection() === 1) {
+        this.xVelocity *= this.randomDirection()
+      } else {
+        this.yVelocity *= this.randomDirection()
+      }
+      this.moves = 0
+    }
+
+    this.moves++
+
     this.draw()
+  }
+
+  infect = (persons) => {
+    for (const person of persons) {
+      if (!person.isInfected) {
+        if (
+          person.x < this.x + this.radius * 2 &&
+          person.x > this.x - this.radius * 2 &&
+          this.id !== person.id
+        ) {
+          if (
+            person.y < this.y + this.radius * 2 &&
+            person.y > this.y - this.radius * 2
+          ) {
+            person.isInfected = Math.ceil(Math.random() * 1000) < 25
+          }
+        }
+      }
+    }
+  }
+
+  randomDirection = () => {
+    return (-1) ** Math.ceil(Math.random() * 2)
   }
 }
--- index.js
+++ index.js
@@ -1,9 +1,29 @@
+const balls = []
+let days = 0
+
 function setup() {
   createCanvas(300, 300)
-  ball = new Ball(150, 150, 9)
+  for (let i = 0; i < 500; i++) {
+    if (i === 1) {
+      balls.push(new Ball(3, i, true))
+    } else {
+      balls.push(new Ball(3, i))
+    }
+  }
 }
 
 function draw() {
   background(240)
-  ball.move()
+  for (const ball of balls) {
+    ball.move()
+    ball.draw()
+    if (ball.isInfected) {
+      ball.infect(balls)
+    }
+  }
+
+  if (frameCount % 30 === 0) {
+    days++
+    console.log(balls.filter((it) => it.isInfected).length + ' days: ' + days)
+  }
 }
